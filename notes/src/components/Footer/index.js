496043fe104541bf02cc5a0d168fecdda2db2744
NithishKumarNithi <nithishkumarnithi19@gmail.com>
2023-03-25 18:54:56 +0530

    React frontend for Notes App

---
+++ notes/src/components/Footer/index.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="bg-light text-center py-2">
+            <div className="fw-semibold"> Copyrights &copy; 2023. All rights reserved.</div>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ notes/src/components/Header/index.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <header className="bg-warning">
+            <div className="container py-3">
+                <h1 className="fs-3 fw-semibold">Notes</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
